390969c5ae7e3458a62c4e6bab0eacecf11f79d0
Jakub Jankiewicz <jcubic@onet.pl>
2020-07-14 10:29:33 +0200

    fix broken css variables

--- colors.js
+++ colors.js
@@ -2,7 +2,7 @@
 
 var hex_re = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i;
 function rgba(hex) {
-  hex = hex.replace(/#([a-f\d])([a-f\d])([a-f\d])/, '#$1$1$2$2$3$3');
+  hex = hex.replace(/^#([a-f\d])([a-f\d])([a-f\d])$/, '#$1$1$2$2$3$3');
   var m = hex.match(hex_re);
   if (m) {
     return `rgba(${m.slice(1).map(num => parseInt(num, 16).toString()).concat(['0.997'])})`;
