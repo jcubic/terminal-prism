c3b8a83574f8073141d444f7e93dd51c3e0e26e7
Jakub Jankiewicz <jcubic@onet.pl>
2022-08-05 23:38:31 +0200

    limit the use of the style to terminal
    Fix the issue when using Terminal Prism on the page
    that alredy use Prism with different style

--- colors.js
+++ colors.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
+var fs = require('fs');
+var data = fs.readFileSync(0, 'utf-8');
+
 var hex_re = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i;
 function rgba(hex) {
   hex = hex.replace(/^#([a-f\d])([a-f\d])([a-f\d])$/, '#$1$1$2$2$3$3');
@@ -8,9 +11,6 @@ function rgba(hex) {
     return `rgba(${m.slice(1).map(num => parseInt(num, 16).toString()).concat(['0.997'])})`;
   }
 }
-
-var fs = require('fs');
-var data = fs.readFileSync(0, 'utf-8');
 
 data = data.toString().replace(/(^|\s)(background-color|color|background)\s*:\s*(#[^;]+)/g, function(_, pre, prop, hex) {
   return `${pre}--${prop}: ${rgba(hex)}`;
